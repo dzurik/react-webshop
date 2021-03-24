c01db1defc6fbd244fae13a3a86f4a7b64908207
dzurik <dzurikskill@hotmail.com>
2021-03-24 18:14:19 +0100

    -

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ import adminReducer from './store/reducers/admin';
 import authReducer from './store/reducers/auth';
 
 const composeEnhancers =
-  process.env.NODE_ENV === 'development'
+  (process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+    : null) || compose;
 
 const rootReducer = combineReducers({
   products: productsReducer,
